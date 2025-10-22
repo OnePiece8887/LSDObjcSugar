af7e50730516f99a20da8640248b2a88b33e30ac
liushuai <liushuai@hnxaca.com>
2025-10-22 15:19:19 +0800

    feat(功能模块): smp

--- Package.swift
+++ Package.swift
@@ -16,7 +16,8 @@ let package = Package(
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "LSDObjcSugar",
-            sources:["LSDObjcSugar/ObjcSugar"],
+            path:"LSDObjcSugar/ObjcSugar",
+            resources: [.copy("PrivacyInfo.xcprivacy")],
             publicHeadersPath: "include"
         )
     ]
