f75380913f30b4aa01978735b6a246b7a0bd2838
liushuai <liushuai@hnxaca.com>
2025-10-22 16:14:24 +0800

    3.1.6

---
+++ Sources/LSDObjcSugar/.h
@@ -0,0 +1,20 @@
+//
+//  Header.h
+//  LSDObjcSugar
+//
+//  Created by yafei li on 2025/10/22.
+//
+
+#ifndef Header_h
+#define Header_h
+
+
+
+#ifdef __OBJC__
+    #import <Foundation/Foundation.h>
+    #import <UIKit/UIKit.h>
+#endif
+
+
+
+#endif /* Header_h */
